d53602d7f441d3590f18cc3ab9ec5d8cdf458d2e
NoobDev11 <67225601+NoobDev11@users.noreply.github.com>
2025-09-26 00:14:15 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,6 +13,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -52,7 +53,10 @@ android {
         }
     } // <-- Closing brace for buildTypes
 } // <-- Closing brace for android
-
+dependencies {
+    implementation("com.android.tools:desugar_jdk_libs:2.0.3")
+    // Add other dependencies if any
+}
 flutter {
     source = "../.."
 }
