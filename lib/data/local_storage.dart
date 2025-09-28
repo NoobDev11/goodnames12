fb28a78bf540e8ce1433e8018a2ddbfb9a1d9146
NoobDev11 <67225601+NoobDev11@users.noreply.github.com>
2025-09-28 12:23:20 +0530

    Update local_storage.dart

--- lib/data/local_storage.dart
+++ lib/data/local_storage.dart
@@ -3,28 +3,28 @@ import 'package:shared_preferences/shared_preferences.dart';
 class LocalStorage {
   static final LocalStorage _instance = LocalStorage._internal();
 
-  factory LocalStorage() {
-    return _instance;
-  }
-
   LocalStorage._internal();
 
-  /// Save a string value for a given [key].
-  /// Optional [prefix] to namespace keys.
+  static LocalStorage get instance => _instance;
+
+  factory LocalStorage() => _instance;
+
+  /// Save a string value for a given key.
+  /// Optional prefix to namespace keys.
   Future<void> setString(String key, String value, {String prefix = ''}) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setString('$prefix$key', value);
   }
 
-  /// Retrieve a string value for a given [key].
-  /// Optional [prefix] to namespace keys.
+  /// Retrieve a string value for a given key.
+  /// Optional prefix to namespace keys.
   Future<String?> getString(String key, {String prefix = ''}) async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getString('$prefix$key');
   }
 
-  /// Remove a string value for a given [key].
-  /// Optional [prefix] to namespace keys.
+  /// Remove a string value for a given key.
+  /// Optional prefix to namespace keys.
   Future<void> remove(String key, {String prefix = ''}) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.remove('$prefix$key');
